cf61484386a805d1fb4cf61d32fc063cb1f1830d
sandy1203 <dhameliyasandeep@gmail.com>
2025-05-22 12:22:42 +0530

    first commit

--- src/components/ChildParentData.vue
+++ src/components/ChildParentData.vue
@@ -1,5 +1,6 @@
 <template>
     <h1>Send Data Child to Parent in VueJS</h1>
+    <h4>{{ childUser }}</h4>
     <User :getUser="getUserName" />
 </template>
 
@@ -9,12 +10,17 @@ import User from './User.vue';
 
 export default {
     name: "ChildParentData",
+    data() {
+        return {
+            childUser: ""
+        }
+    },
     components: {
         User
     },
     methods: {
         getUserName(name) {
-            alert(name)
+            this.childUser = name
         }
     }
 }
